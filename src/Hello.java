864a7d963ead5de57a972bc487babb2f62a33bd8
rileymiklos <rileymiklos@fe80:0:0:0:144f:60b8:36ae:98dd%11>
2024-08-28 18:11:11 -0500

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Riley Miklos
+ * Date: 08/28/2024
+ * 
+ * A simple program to write my name
+ * 
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Riley Miklos");
+		
+	}
+
+}
